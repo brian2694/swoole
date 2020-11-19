3c8b7e618801ffd3da60f04a04a2492f8f63302c
Brian S <brian2694@hotmail.com>
2020-11-19 02:27:13 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", "BasicController@index");
+Route::group([
+    'namespace'  => 'App\Http\Controllers\Api',
+], function () {
+    Route::get("/", "BasicController@index");
+});
